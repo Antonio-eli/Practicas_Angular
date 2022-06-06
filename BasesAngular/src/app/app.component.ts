110e4d8b210757655213deca59743c1fc99bc1cc
Antonio-eli <eliceoroan@hotmail.com>
2022-06-05 22:13:46 -0500

    Contador App evento (click) Contador optimizacion de codigo  Contador tarea  Para crear componentes de manera manual se debe de enlazar con el app.module.ts

---
+++ BasesAngular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  /*
+    Nombre del componente que v en el archivo principal
+    index.html <app-root></app-root>
+  */
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
