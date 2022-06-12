bc3e139e4f022f5a40df58cf738a369d6eb39658
Antonio-eli <eliceoroan@gmail.com>
2022-06-12 16:46:29 -0500

    Add Concepto de one way data binding

--- BasesAngular/src/app/heroes/heroe/heroe.component.ts
+++ BasesAngular/src/app/heroes/heroe/heroe.component.ts
@@ -11,12 +11,20 @@ export class HeroeComponent{
     nombre: string = 'Ironman';
     edad  : number = 43
 
-    get nombreCapitalizado(){
+    get nombreCapitalizado(): string{
         return this.nombre.toUpperCase();
     }
 
     obtenerNombre(): string{
         return ` ${this.nombre} - ${this.edad} `
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spidernan';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 50;
     }
 
 }
