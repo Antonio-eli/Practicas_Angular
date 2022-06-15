503c3a1d0b59c96da9216c584c5930ce9e0e5460
Antonio-eli <eliceoroan@gmail.com>
2022-06-14 23:48:38 -0500

    creacion de interface y vinculacion de datos con [(ngModel)]="nuevo.nombre"

--- BasesAngular/src/app/dbz/main-page/main-page.component.ts
+++ BasesAngular/src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre : 'Son Gokū',
+    poder  : 100000
+  }
+
   agregar() {
-    console.log('Heyyyyyyyy');
+    console.log(this.nuevo.nombre);
+    console.log(this.nuevo.poder);
   }
 
 }
