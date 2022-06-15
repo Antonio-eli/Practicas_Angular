0f3cbb985901ad400b0fce8e027641ada7d90660
Antonio-eli <eliceoroan@gmail.com>
2022-06-15 00:25:03 -0500

    directiva *ngFor

--- BasesAngular/src/app/dbz/main-page/main-page.component.ts
+++ BasesAngular/src/app/dbz/main-page/main-page.component.ts
@@ -11,14 +11,40 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Son Gokū',
+      poder: 50000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 45000
+    },
+    {
+      nombre: 'Bardock',
+      poder: 10000
+    },
+    {
+      nombre: 'Rey Vegeta',
+      poder: 5000
+    }
+  ];
+
+  deletePersonaje: string = '';
+  addPersonaje: string = '';
   nuevo: Personaje = {
-    nombre : 'Son Gokū',
-    poder  : 100000
+    nombre : '',
+    poder  : 0
   }
 
   agregar() {
-    console.log(this.nuevo.nombre);
-    console.log(this.nuevo.poder);
+    if (this.nuevo.nombre.trim().length === 0) { return; }
+    console.log(this.nuevo);
+    this.personajes.push( this.nuevo );
+    this.nuevo = {
+      nombre : '',
+      poder  : 0
+    }
   }
 
 }
