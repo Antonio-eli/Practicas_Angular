37e4acb599a43b813c61b45eacab074b72e1e1f8
Antonio-eli <eliceoroan@gmail.com>
2022-06-18 14:17:37 -0500

    Add @Output(), sirve para emitir eventos

--- BasesAngular/src/app/dbz/main-page/main-page.component.ts
+++ BasesAngular/src/app/dbz/main-page/main-page.component.ts
@@ -30,7 +30,10 @@ export class MainPageComponent {
     poder: 10
   }
 
-  deletePersonaje: string = '';
-  addPersonaje: string = '';
+  agregarNuevoPersonaje( argumento: Personaje ){
+    console.log('Main page component: click agregarNuevoPersonaje, El evento de fue emitio por el  hijo lo recibio el padre y lo interpreto');
+    console.log('%c Argumento :: ', 'background: #222; color: #bada55', argumento);
+    this.personajes.push( argumento );
+  }
 
 }
