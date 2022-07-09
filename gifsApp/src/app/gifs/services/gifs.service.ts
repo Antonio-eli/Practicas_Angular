12e59cf7877e9c9d56a5518a5c866030e6132b23
Antonio-eli <eliceoroan@gmail.com>
2022-07-09 16:09:07 -0500

    Add localStorage

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Gif, SearchGifsaResponse } from '../interface/gifs.interface';
+import { json } from 'express';
 
 @Injectable({
   providedIn: 'root'
@@ -16,13 +17,20 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient) {}
+  constructor( private http: HttpClient) {
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+    /*if ( localStorage.getItem('historial') ) {
+      this._historial = JSON.parse( localStorage.getItem('historial')! );
+    }*/
+  }
 
   buscarGifs( query: string = '') {
     query = query.trim().toLocaleLowerCase();
     if (!this._historial.includes( query )) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(  this._historial ));
     }
     console.log(this._historial);
     this.http.get<SearchGifsaResponse>(`https://api.giphy.com/v1/gifs/search?api_key=f4MdoV7pknpWEZFEx364O4vunJIWsLUR&q=${ query }&limit=10`)
