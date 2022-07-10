2028de214e298c3a69ca14f1194e2f32fffa7b1e
Antonio-eli <eliceoroan@gmail.com>
2022-07-10 12:32:05 -0500

    refactor HttpParams

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGifsaResponse } from '../interface/gifs.interface';
-import { json } from 'express';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private     apikey: string  = 'f4MdoV7pknpWEZFEx364O4vunJIWsLUR';
-  private _historial: string[] = [];
-  public resultados : Gif[] = [];
+  private      apikey: string  = 'f4MdoV7pknpWEZFEx364O4vunJIWsLUR';
+  private servicioUrl: string  = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
+
+  public resultados: Gif[] = [];
 
   // TODO: Cambiar any por su tipado correspondiente
   get historial() {
@@ -34,7 +35,12 @@ export class GifsService {
       localStorage.setItem('historial', JSON.stringify(  this._historial ));
     }
     console.log(this._historial);
-    this.http.get<SearchGifsaResponse>(`https://api.giphy.com/v1/gifs/search?api_key=f4MdoV7pknpWEZFEx364O4vunJIWsLUR&q=${ query }&limit=10`)
+    const params = new HttpParams()
+                                  .set('api_key', this.apikey)
+                                  .set('limit', '10')
+                                  .set('q', query);
+    console.log(params.toString());
+    this.http.get<SearchGifsaResponse>(`${ this.servicioUrl }/search`, { params } )
               .subscribe( (resp) => {
                 console.log(resp.data);
                 this.resultados = resp.data;
