6a52f93b03cfbd74ce59ded36486375d40a39319
Antonio-eli <eliceoroan@gmail.com>
2022-11-21 21:23:10 -0600

    Add modelos (Interface)

--- spotify-a/src/app/shared/components/media-player/media-player.component.ts
+++ spotify-a/src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '../../../core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover: any = {
+  mockCover: TrackModel = {
     cover: 'https://media.pitchfork.com/photos/627425dbc85171592b8a6e6a/1:1/w_600/Bad-Bunny-Un-Verano-Sin-Ti.jpg',
     album: 'Un Verano Sin Ti',
-    name: 'Bad Bunny '
+    name: 'Bad Bunny ',
+    url: 'https://www.youtube.com/watch?v=z8xLN07Pt_8&list=RDz8xLN07Pt_8&start_radio=1',
+    _id: '1'
   }
   constructor() { }
 
