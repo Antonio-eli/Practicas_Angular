1bdd5ede8d56b85cb7624ba17ceeaf05b963fd7b
Antonio-eli <eliceoroan@gmail.com>
2022-11-26 15:26:33 -0600

    Add queryParams

--- spotify-a/src/app/shared/components/side-bar/side-bar.component.ts
+++ spotify-a/src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -12,7 +13,7 @@ export class SideBarComponent implements OnInit {
   } = { defaultOptions: [], accessLink: []}
 
   custumOptions: Array<any> = []
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.mainMenu.defaultOptions = [
@@ -29,7 +30,8 @@ export class SideBarComponent implements OnInit {
       {
         name: 'Tu biblioteca',
         icon: 'uil uil-chart',
-        router: ['/', 'favorites']
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo'}
       }
     ]
 
@@ -62,6 +64,15 @@ export class SideBarComponent implements OnInit {
         router: ['/']
       },
     ]
+  }
+
+  goTo($event: any):void{
+    this.router.navigate(['/', 'favorites'], {queryParams:{
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3'
+    }})
+    console.log($event);
   }
 
 }
